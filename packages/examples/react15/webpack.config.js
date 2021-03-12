99b963fd589e7682a831627c317df9118cb8d001
youshiyu <youshiyu@corp.netease.com>
2021-03-12 18:12:29 +0800

    chore:删除一些文件

---
+++ packages/examples/react15/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  // mode: 'production',
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    port: 9528,
+    headers:{"Access-Control-Allow-Origin":'*'},
+    open: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template:'./src/index.html',
+      minify: false,
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean:true,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
